9432b20afb75123e52846d8088c274040a0cb9f1
Smilodon PC <ibraimweb@gmail.com>
2017-07-26 01:26:50 -0400

    changes

--- app/staff.js
+++ app/staff.js
@@ -11,13 +11,12 @@ context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 // Create a stave of width 400 at position 10, 40 on the canvas.
 var stave = new VF.Stave(60, 10, 250);
 // Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
+stave.addClef("treble").addTimeSignature("11/4");
 
 // Connect it to the rendering context and draw!
 stave.setContext(context).draw();
 
-// var notes = [ new VF.StaveNote({ keys: ["g/3", "c/4", "eb/4"], duration: "q" }).addAccidental(0, new VF.Accidental("b")), 
-//               new VF.StaveNote({ keys: ["c/4", "eb/4", "g/4"], duration: "q" }).addAccidental(0, new VF.Accidental("b")) ];
+var notes = [];
 
 VF.Formatter.FormatAndDraw(context, stave, notes);
 
